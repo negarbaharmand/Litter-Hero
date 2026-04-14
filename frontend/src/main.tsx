440620e3841c00075db7f77017dbeb851cefffd1
John Doe <hugo.oblak@outlook.com>
2026-04-14 18:39:53 +0200

    created the login page and added the necessary dependencies for the frontend. Updated the App component to include routing for the login page and made some styling adjustments.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-
-// 1. Add these imports
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-// 2. Create the queryClient
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* 3. Wrap App with the Provider */}
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
