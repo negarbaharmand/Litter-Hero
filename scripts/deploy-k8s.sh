6ecf1b60fa06c0ddc2cc63b387cd469433daa89b
piensoho@gmail.com <piensoho@gmail.com>
2026-05-20 17:43:14 +0200

    forgot to add namesuffix on all the deployments, ingressess etc. that is fixed now. Also moved nginx.conf to configmap instead. hopefully everything will work as inteded now

--- scripts/deploy-k8s.sh
+++ scripts/deploy-k8s.sh
@@ -7,6 +7,7 @@ echo "$KUBECONFIG_B64" | base64 -d >/kubeconfig
 export KUBECONFIG=/kubeconfig
 
 kubectl apply -n "${NAMESPACE}" -f k8s/10-configmap-backend.yml
+envsubst '${CI_COMMIT_REF_SLUG}' <k8s/11-nginx-configmap.yml | kubectl apply -n "${NAMESPACE}" -f -
 
 kubectl apply -n "${NAMESPACE}" -f k8s/20-gitlab-secrets.yml \
   -f k8s/21-cc-secrets.yml \
@@ -15,17 +16,18 @@ kubectl apply -n "${NAMESPACE}" -f k8s/20-gitlab-secrets.yml \
 kubectl apply -n "${NAMESPACE}" -f k8s/30-deploy-database.yml
 kubectl rollout status statefulset database -n "${NAMESPACE}" --timeout=5m
 
-envsubst '${CI_REGISTRY_IMAGE} ${CI_COMMIT_SHA}' <k8s/40-deploy-backend.yml | kubectl apply -n "${NAMESPACE}" -f -
-kubectl rollout status deployment backend -n "${NAMESPACE}" --timeout=5m
+envsubst '${CI_COMMIT_REF_SLUG} ${CI_REGISTRY_IMAGE} ${CI_COMMIT_SHA}' <k8s/40-deploy-backend.yml | kubectl apply -n "${NAMESPACE}" -f -
+kubectl rollout status deployment backend-"${CI_COMMIT_REF_SLUG}" -n "${NAMESPACE}" --timeout=5m
 
-kubectl delete job backend-migrate -n "${NAMESPACE}" --ignore-not-found
-envsubst '${CI_REGISTRY_IMAGE} ${CI_COMMIT_SHA}' <k8s/50-migrate-db-job.yml | kubectl apply -n "${NAMESPACE}" -f -
+kubectl delete job backend-migrate-"${CI_COMMIT_REF_SLUG}" -n "${NAMESPACE}" --ignore-not-found
 
-envsubst '${CI_REGISTRY_IMAGE} ${CI_COMMIT_SHA}' <k8s/60-deploy-frontend.yml | kubectl apply -n "${NAMESPACE}" -f -
-kubectl rollout status deployment frontend -n "${NAMESPACE}" --timeout=5m
+envsubst '${CI_COMMIT_REF_SLUG} ${CI_REGISTRY_IMAGE} ${CI_COMMIT_SHA}' <k8s/50-migrate-db-job.yml | kubectl apply -n "${NAMESPACE}" -f -
+
+envsubst '${CI_COMMIT_REF_SLUG} ${CI_REGISTRY_IMAGE} ${CI_COMMIT_SHA}' <k8s/60-deploy-frontend.yml | kubectl apply -n "${NAMESPACE}" -f -
+kubectl rollout status deployment frontend-"${CI_COMMIT_REF_SLUG}" -n "${NAMESPACE}" --timeout=5m
 
 kubectl apply -n "${NAMESPACE}" -f k8s/70-middleware.yml
 
-envsubst '${FRONTEND_HOST} ${BACKEND_HOST}' <k8s/80-ingress.yml | kubectl apply -n "${NAMESPACE}" -f -
+envsubst '${CI_COMMIT_REF_SLUG} ${FRONTEND_HOST} ${BACKEND_HOST}' <k8s/80-ingress.yml | kubectl apply -n "${NAMESPACE}" -f -
 
 kubectl apply -n "${NAMESPACE}" -f k8s/90-monitoring.yml
